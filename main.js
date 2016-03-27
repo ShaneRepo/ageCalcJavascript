a370c5ac5d6233094822b7b35717e4071530cf7e
Shane <shane8641@live.com>
2016-03-27 09:20:17 -0400

    gathering value supplied by the user and storing them in variables.

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
 function calcAge() {
+    var today = new Date();
+
     var userYear = document.getElementById("year").value;
     var outputYear = document.getElementById("years");
-    outputYear.value = userYear;
-    /*document.getElementById("days").innerHTML = uDay.toString();
-    document.getElementById("hours").innerHTML = uDay.toString();
-    document.getElementById("minutes").innerHTML = uDay.toString();
-    document.getElementById("bday").innerHTML = uDay.toString();*/
-    /*alert(today.getMonth());*/
+    var userMonth = document.getElementById("month").value;
+    var outputMonth = document.getElementById("months");
+    var userDay = document.getElementById("day").value;
+    var outputDay = document.getElementById("days");
+
+    var userBDAY = new Date(userYear,userMonth,userDay);
+
+
+
+    outputYear.value = today.getFullYear() - userYear;
+
 }
 
 
