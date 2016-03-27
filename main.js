b625a49e0dcd32f232a4731c2250b46f3501acd8
Shane <shane8641@live.com>
2016-03-26 21:12:09 -0400

    figuring out how to update results using value not innerHTML. :)

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
+function calcAge() {
+    var userYear = document.getElementById("year").value;
+    var outputYear = document.getElementById("years");
+    outputYear.value = userYear;
+    /*document.getElementById("days").innerHTML = uDay.toString();
+    document.getElementById("hours").innerHTML = uDay.toString();
+    document.getElementById("minutes").innerHTML = uDay.toString();
+    document.getElementById("bday").innerHTML = uDay.toString();*/
+    /*alert(today.getMonth());*/
+}
+
+
+
+
+
+
+
+
+
+
+
 
