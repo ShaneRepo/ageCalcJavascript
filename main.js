9bf629398c46eaa3c05c9fb0c97e40648f1d2c45
Shane <shane8641@live.com>
2016-03-27 10:15:36 -0400

    calculated user's age in months.

--- main.js
+++ main.js
@@ -7,15 +7,22 @@ function calcAge() {
     var outputMonth = document.getElementById("months");
     var userDay = document.getElementById("day").value;
     var outputDay = document.getElementById("days");
-
+    var outputHours = document.getElementById("hours");
+    var outputMinutes = document.getElementById("minutes");
     var userBDAY = new Date(userYear,userMonth,userDay);
+    var monthCalc = userBDAY.getMonth() + 1;
 
 
     if (userBDAY.getMonth() > today.getMonth()){
+        var yearCalc = (today.getFullYear() - userBDAY.getFullYear()) - 1;
         outputYear.value = (today.getFullYear() - userBDAY.getFullYear()) - 1;
+        outputMonth.value = (yearCalc * 12) + monthCalc;
     } else {
+        var yearCalc2 = (today.getFullYear() - userBDAY.getFullYear());
         outputYear.value = today.getFullYear() - userBDAY.getFullYear();
+        outputMonth.value = (yearCalc2 * 12) - monthCalc;
     }
+
 
 
 }
